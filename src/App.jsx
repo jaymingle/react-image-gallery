20c9830d3b8c76c4213e88499d3521026633ec54
Jay Mingle <jkawudi@gmail.com>
2024-01-29 01:36:17 +0000

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,28 @@ function App() {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-xl">
         <img className="w-full" src="https://source.unsplash.com/random/" alt=""/>
-        <h1 className="text-red-700 text-4xl font-bold underline italic">Hello World</h1>
-        <div className="px-6 py-4"></div>
-        <div className="font-bold text-purple-500 text-xl"></div>
+        <div className="px-6 py-4">
+            <div className="font-bold text-purple-500 text-xl">Photo by John Doe</div>
+            <div>
+                <ul>
+                    <li>
+                        <strong>Views: </strong>
+                        4000
+                    </li>
+                    <li>
+                        <strong>Dowloads: </strong>
+                        4000
+                    </li>
+                    <li>
+                        <strong>Likes: </strong>
+                        4000
+                    </li>
+                </ul>
+            </div>
+            <div className="px-6 py-4">
+                span.inline-block.bg-gray-200.rounded-full.px-3.py-1
+            </div>
+        </div>
     </div>
   )
 }
