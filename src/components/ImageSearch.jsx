053ea6e0ca109fa5ae6df8bcca51623900e4d612
Jay Mingle <jkawudi@gmail.com>
2024-01-29 03:47:31 +0000

    Changes

--- src/components/ImageSearch.jsx
+++ src/components/ImageSearch.jsx
@@ -1,10 +1,18 @@
+import React, {useState} from 'react'
+
 const ImageSearch = () => {
     return(
         <div>
-            <form action="">
-                <input type="text" placeholder="Search Image" className="border-1 border-black"/>
-                <input type="submit" value="Search"/>
-            </form>
+            <div className='max-w-sm rounded overflow-hidden my-10 mx-auto'>
+                <form onSubmit={onSubmitHandler} className="w-full max-w-sm">
+                    <div className="flex items-center border-b border-b-2 border-teal-500 py-2">
+                        <input onChange={searchTextHandler} className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Search Image Term..." />
+                        <button className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="submit">
+                            Search
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
