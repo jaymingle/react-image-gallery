da1f96e9bdb0adba40eee8c069e0f66c07512f15
Jay Mingle <jkawudi@gmail.com>
2024-01-29 03:40:53 +0000

    Changes

---
+++ src/components/ImageSearch.jsx
@@ -0,0 +1,12 @@
+const ImageSearch = () => {
+    return(
+        <div>
+            <form action="">
+                <input type="text" placeholder="Search Image" className=""/>
+                <input type="submit" value="Search"/>
+            </form>
+        </div>
+    )
+}
+
+export default ImageSearch;
