342ef69bc668dbf3edf8c4d54b29ec8ce3b1e974
Jay Mingle <jkawudi@gmail.com>
2024-01-29 01:30:21 +0000

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ function App() {
     <div className="max-w-sm rounded overflow-hidden shadow-xl">
         <img className="w-full" src="https://source.unsplash.com/random/" alt=""/>
         <h1 className="text-red-700 text-4xl font-bold underline italic">Hello World</h1>
+        <div className="px-6 py-4"></div>
+        <div className="font-bold text-purple-500 text-xl"></div>
     </div>
   )
 }
