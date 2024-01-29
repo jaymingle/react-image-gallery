1d09c8a5037ce27da72ed5a7af43faed69ffcac9
Jay Mingle <jkawudi@gmail.com>
2024-01-29 02:50:39 +0000

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -36,33 +36,9 @@ useEffect(() => {
     }
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-xl">
-        <img className="w-full" src="https://source.unsplash.com/random/" alt=""/>
-        <div className="px-6 py-4">
-            <div className="font-bold text-purple-500 text-xl">Photo by John Doe</div>
-            <div>
-                <ul>
-                    <li>
-                        <strong>Views: </strong>
-                        4000
-                    </li>
-                    <li>
-                        <strong>Dowloads: </strong>
-                        4000
-                    </li>
-                    <li>
-                        <strong>Likes: </strong>
-                        4000
-                    </li>
-                </ul>
-            </div>
-            <div className="px-6 py-4">
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag1</span>
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag2</span>
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag3</span>
-            </div>
-        </div>
-    </div>
+   <div>
+
+   </div>
   )
 }
 
