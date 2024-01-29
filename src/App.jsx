5d34283a72235069e0b5463b67a5c97b6ec33fc2
Jay Mingle <jkawudi@gmail.com>
2024-01-29 00:50:56 +0000

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-        <h1 className="text-yellow-300">Hello World</h1>
+        <h1 className="text-red-700 text-4xl font-bold underline italic">Hello World</h1>
     </>
   )
 }
